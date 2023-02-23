837aad32644243b9b9da00de418d575d57488e3a
Jon Turney <jon.turney@dronecode.org.uk>
2023-02-23 13:42:37 +0000

    Fix inclusion of iphlpapi.h on Cygwin

--- include/net-snmp/system/cygwin.h
+++ include/net-snmp/system/cygwin.h
@@ -1,1 +1,3 @@
-/* Everything that is specific to Cygwin is detected by the configure script */
+// We can't include winsock.h on Cygwin, because it clashes with socket.h
+// so define here the Win32 API types required by iphlpapi.h
+typedef struct in6_addr IN6_ADDR;
